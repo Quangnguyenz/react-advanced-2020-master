fd50db98840073aea2c78e8c8b15744ed806dc47
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-11-11 09:10:09 -0600

    Update

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,11 +9,20 @@ const ControlledInputs = () => {
 
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
-
+  const [people, setPeople] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email)
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email }
+      setPeople((people) => {
+        return [...people, person]
+      });
+      // setFirstName('')
+      // setEmail('')
+    } else {
+      console.log('empty values')
+    }
   }
 
   return <>
@@ -29,6 +38,15 @@ const ControlledInputs = () => {
         </div>
         <button type='submit'>add person</button>
       </form>
+      {
+        people.map((person) => {
+          const { id, firstName, email } = person
+          return <div key={id} className='item'>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        })
+      }
     </article>
   </>;
 };
