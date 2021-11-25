72f90aa51e3f4c7dd0559510f07087d8055eb451
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-11-25 10:39:41 -0600

    Added params to router and set up links

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,22 +10,25 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <Router>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/people">
-        <People />
-      </Route>
-      <Route path="*">
-        <Error />
-      </Route>
-    </Switch>
-  </Router>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="/person/:id" children={<Person />}></Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>);
 };
 
 export default ReactRouterSetup;
